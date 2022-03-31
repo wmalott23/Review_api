8dcbc379d99a32e0eb863cb574d4b5f72804fa4f
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-31 15:06:43 -0700

    Stopped at some issues with PUT commands in postman

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.reviews_list), 
-    path ('<int:pk>', views.names_detail),
-    path('', views.products_list), 
-    path ('<int:pk>', views.products_detail),
+    path('reviews/', views.reviews_list), 
+    path ('reviews/<int:pk>', views.reviews_detail),
+    path('products/', views.products_list), 
+    path ('products/<int:pk>', views.products_detail),
 ]
