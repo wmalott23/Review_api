5f4d9737d2efa785627e6eb2c12a0acbe136d445
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-31 13:59:13 -0700

    Took me waaay too long to figure out that the project folder was in another project folder, also migrate was in said additional folder, moved things to right place, then migrated and created app folder

--- reviews_project/settings.py
+++ reviews_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,3 +109,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
+
+try: 
+    from reviews_project.local_settings import *
+except ImportError:
+    pass
