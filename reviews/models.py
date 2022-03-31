caaabbca55776e1838e077684a077fb4de436ce6
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-31 14:23:20 -0700

    Created product and reviews models, assigned product as foreign key to review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Review(models.Model):
+    prod_num = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    verbage = models.CharField(max_length=255)
+
